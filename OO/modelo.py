94e80a00928d09052b25707e61983258bff9c02c
Samuel Paes <samuel.paes@propig.com.br>
2021-01-09 23:20:42 -0300

    Exemplo de utilização de polimorfismo, responsividade da classe, e sobrescrita do método __str__ para representar os objetos em forma de texto

--- OO/modelo.py
+++ OO/modelo.py
@@ -19,11 +19,17 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
 
 
 class Serie(Programa):
@@ -31,6 +37,8 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -41,5 +49,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+listinha = [atlanta, vingadores]
+
+for programa in listinha:
+    print(programa)
