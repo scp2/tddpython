5cabfdc49e8037b7775efee087791502e56bed63
Samuel Paes <samuel.paes@propig.com.br>
2021-01-09 23:44:51 -0300

    definição do método __iter__

--- OO/modelo.py
+++ OO/modelo.py
@@ -22,14 +22,14 @@ class Programa:
     def __str__(self):
         return f'Nome: {self.nome} Likes: {self.likes}'
 
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-    
+
     def __str__(self):
         return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
-
 
 
 class Serie(Programa):
@@ -39,6 +39,24 @@ class Serie(Programa):
 
     def __str__(self):
         return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+
+
+class PlayList:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __iter__(self):
+        return iter(self._programas)
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -50,6 +68,7 @@ atlanta.dar_likes()
 atlanta.dar_likes()
 
 listinha = [atlanta, vingadores]
+minha_play_list = PlayList('Minha Play list', listinha)
 
-for programa in listinha:
+for programa in minha_play_list:
     print(programa)
